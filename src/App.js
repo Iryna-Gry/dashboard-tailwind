d88798eeab5d8560eb7dbd173410ee7b8d8ab76b
Iryna Grytsaenko <iryna.grytsaenko@gmail.com>
2023-03-24 09:16:03 +0200

    adds sidebar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { FiSettings } from "react-icons/fi";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
@@ -23,14 +23,21 @@ import {
   Line,
 } from "./pages";
 import "./App.css";
+import { useStateContext } from "./contexts/ContextProvider";
 const App = () => {
-  const activeMenu = true;
+  const { activeMenu } = useStateContext();
+
   return (
     <div>
       <BrowserRouter>
         <div className="flex relative dark:bg-main-dark-bg">
           <div className="fixed right-4 bottom-4" style={{ zIndex: 1000 }}>
-            <TooltipComponent content="Settings" position="Top">
+            <TooltipComponent
+              content="Settings"
+              position="Top"
+              className="bg-red-600"
+              cssClass="customtooltip"
+            >
               <button
                 type="button"
                 className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
