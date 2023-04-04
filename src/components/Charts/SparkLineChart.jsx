894b345f8562f8a7eb6cd91a445551ae24a29127
Iryna Grytsaenko <iryna.grytsaenko@gmail.com>
2023-04-04 16:31:42 +0300

    Adds Ecommerce and NavBar

--- src/components/Charts/SparkLineChart.jsx
+++ src/components/Charts/SparkLineChart.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import {
+  SparklineComponent,
+  Inject,
+  SparklineTooltip,
+} from "@syncfusion/ej2-react-charts";
 
-const SparkLineChart = () => {
-  return <div>SparkLineChart</div>;
+const SparkLine = ({ currentColor, id, type, height, width, data, color }) => {
+  return (
+    <SparklineComponent
+      id={id}
+      height={height}
+      width={width}
+      lineWidth={1}
+      valueType="Numeric"
+      fill={color}
+      border={{ color: currentColor, width: 2 }}
+      dataSource={data}
+      xName="x"
+      yName="yval"
+      type={type}
+      tooltipSettings={{
+        visible: true,
+        format: "${x} : data ${yval}",
+        trackLineSettings: { visible: true },
+      }}
+    >
+      <Inject services={[SparklineTooltip]} />
+    </SparklineComponent>
+  );
 };
 
-export default SparkLineChart;
+export default SparkLine;
